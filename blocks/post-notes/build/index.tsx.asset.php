83f3272a7e31503d0956447bba08712c6abce4ca
Puleeno Nguyen <puleeno@gmail.com>
2026-09-25 13:25:09 +0700

    feat: add border support and styles to post notes block

--- blocks/post-notes/build/index.tsx.asset.php
+++ blocks/post-notes/build/index.tsx.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '391671aa6c60cac3bc98');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'b04a8b3e045a8bdc0315');
